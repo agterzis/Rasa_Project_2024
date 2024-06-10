60b068ff032ace879828d39658fd17f51d353526
agterzis <agterzis@csd.auth.gr>
2024-06-10 16:34:42 +0300

    .

--- actions/actions.py
+++ actions/actions.py
@@ -1,18 +1,22 @@
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet  # Import SlotSet
 
 
-class ActionHelloWorld(Action):
-
+class ActionCheckOrderId(Action):
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_check_order_id"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hello World!")
+        order_id = next(tracker.get_latest_entity_values("id"), None)
 
-        return []
+        if order_id == "12345":
+            dispatcher.utter_message(text="Your order will arrive in one day.")
+            return []
+        else:
+            dispatcher.utter_message(text="Incorrect ID, please try again.")
+            return []
