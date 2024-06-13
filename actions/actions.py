3161321a6dfbfb90021e06d0f8a5d600884b1a92
agterzis <agterzis@csd.auth.gr>
2024-06-13 12:37:36 +0300

    .

--- actions/actions.py
+++ actions/actions.py
@@ -12,27 +12,26 @@ class ActionCheckOrderId(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         order_id = tracker.get_slot("id")
-        # intent_name = tracker.latest_message['intent'].get('name')
-        # current_story = tracker.get_slot("current_story")
         exchange_requested = tracker.get_slot("exchange")  # Παίρνουμε την τιμή του slot
         refund_requested = tracker.get_slot("refund")  # Παίρνουμε την τιμή του slot
 
-        if refund_requested:  # Λογική για επιστροφή χρημάτων
-            dispatcher.utter_message(text=f"Okay, a refund will be issued for the order with ID {order_id}. "
-                                          f"Can I help you with something else? ")
-            return []
-        elif exchange_requested:  # Λογική για ανταλλαγή
-            dispatcher.utter_message(text=f"Nice, I'll change your order with ID {order_id} with a new item. What "
-                                          f"would you like to buy?")
-            return []
-        else:
-            if order_id == "12345":
-                dispatcher.utter_message(text=f"Your order with ID: {order_id} will arrive in one day.")
+        if order_id == "12345":
+
+            if refund_requested:  # Λογική για επιστροφή χρημάτων
+                dispatcher.utter_message(text=f"Okay, a refund will be issued for the order with ID {order_id}. "
+                                              f"Can I help you with something else? ")
+                return []
+            elif exchange_requested:  # Λογική για ανταλλαγή
+                dispatcher.utter_message(text=f"Nice, I'll change your order with ID {order_id} with a new item. What "
+                                              f"would you like to buy?")
                 return []
             else:
-                dispatcher.utter_message(text=f"Sorry, I couldn't find any order with ID {order_id}. Can I help you "
-                                              f"with something else?")
+                dispatcher.utter_message(text=f"Your order with ID: {order_id} will arrive in one day.")
                 return []
+        else:
+            dispatcher.utter_message(text=f"Sorry, I couldn't find any order with ID {order_id}. Can I help you "
+                                          f"with something else?")
+            return []
 
 
 class ActionCheckStoreLocation(Action):
